be93701fa52af250f679d55bffce02c4fce487f1
Stefan <stefam1996@live.com>
2019-06-20 16:39:56 +0200

    stefantflc migration

--- src/main/java/t/stefan/portfolio/entity/RoleName.java
+++
@@ -1,7 +0,0 @@
-package t.stefan.portfolio.entity;
-
-public enum RoleName {
-	ROLE_ADMINISTRATOR,
-	ROLE_MODERATOR,
-	ROLE_USER
-}
--- src/main/java/t/stefan/portfolio/security/JwtAuthenticationException.java
+++
@@ -1,8 +0,0 @@
-package t.stefan.portfolio.security;
-
-public class JwtAuthenticationException extends RuntimeException {
-
-	public JwtAuthenticationException(String message, Throwable cause) {
-		super(message, cause);
-	}
-}
