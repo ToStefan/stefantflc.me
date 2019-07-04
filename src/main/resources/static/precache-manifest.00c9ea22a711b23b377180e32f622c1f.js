3210207de36ba54ce9bc63304d7c1b6810ac288a
Stefan <stefam1996@live.com>
2019-07-04 18:56:46 +0200

    react portfolio init

---
+++ src/main/resources/static/precache-manifest.00c9ea22a711b23b377180e32f622c1f.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "ae6aae46a5de35fbe631",
+    "url": "/static/js/main.147e4ceb.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "30ea74dc1f9718b7e883",
+    "url": "/static/js/2.39c58dfb.chunk.js"
+  },
+  {
+    "revision": "8df5294899e8fe830379d9d2ef1dc640",
+    "url": "/index.html"
+  }
+];
