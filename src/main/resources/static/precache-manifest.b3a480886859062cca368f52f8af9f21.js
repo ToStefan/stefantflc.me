5f0ac7151eb7713fadd9bcb72a79ac162031e42a
Stefan <stefam1996@live.com>
2019-08-04 17:44:05 +0200

    portfolio updated & redesigned

---
+++ src/main/resources/static/precache-manifest.b3a480886859062cca368f52f8af9f21.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "2add3915117f46560ec0",
+    "url": "/static/css/main.fc2014aa.chunk.css"
+  },
+  {
+    "revision": "2add3915117f46560ec0",
+    "url": "/static/js/main.93df5d87.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "a6b1ed2292a0198cb08d",
+    "url": "/static/js/2.cb6dd810.chunk.js"
+  },
+  {
+    "revision": "d82de9e0c77dcd8f468c7e826f299f8b",
+    "url": "/static/media/snake.d82de9e0.gif"
+  },
+  {
+    "revision": "2f722b8142be177c49e0911e4534f834",
+    "url": "/static/media/work-in-progress.2f722b81.png"
+  },
+  {
+    "revision": "d69ac298d30fe46a3a32e5e94bbf308c",
+    "url": "/static/media/portfolio-bg.d69ac298.jpg"
+  },
+  {
+    "revision": "8b792eec6ed728c47204ebc23bb5c75c",
+    "url": "/index.html"
+  }
+];
