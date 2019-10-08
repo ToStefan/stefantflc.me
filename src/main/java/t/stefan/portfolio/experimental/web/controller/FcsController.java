5e0f26dba932859780eb2e767672e951171e9e17
Stefan <stefam1996@live.com>
2019-10-09 00:42:39 +0200

    crawler init - fix 2

--- src/main/java/t/stefan/portfolio/experimental/web/controller/FcsController.java
+++ src/main/java/t/stefan/portfolio/experimental/web/controller/FcsController.java
@@ -1,20 +1,20 @@
 package t.stefan.portfolio.experimental.web.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import lombok.AllArgsConstructor;
 import t.stefan.jython.bridge.CrawlerService;
 
-@AllArgsConstructor
 @RestController
-@RequestMapping(value = "/api/fcs")
+@RequestMapping(value = "/experimental/fcs")
 public class FcsController {
 	
+	@Autowired
 	@Qualifier("crawler")
-	private final CrawlerService crawler;
+	private CrawlerService crawler;
 	
 	@GetMapping(value = "/hello")
 	public String index() {
