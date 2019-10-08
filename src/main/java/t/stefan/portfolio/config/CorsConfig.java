e4726af4b3df0d65a3f957c245758d58a500ed44
Stefan <stefam1996@live.com>
2019-10-08 21:06:14 +0200

    cache purge

--- src/main/java/t/stefan/portfolio/config/CorsConfig.java
+++ src/main/java/t/stefan/portfolio/config/CorsConfig.java
@@ -9,7 +9,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 @Configuration
 public class CorsConfig {
 
-    @Bean
+	@Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurerAdapter() {
             @Override
